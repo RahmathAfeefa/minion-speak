4206e8f874f2c2dd7c4e1d07916990b23d8ebb7c
RahmathAfeefa <rahmathafeefa34@gmail.com>
2022-04-16 16:40:46 +0530

    minions talk done

--- app.js
+++ app.js
@@ -2,12 +2,38 @@ var btntranslate = document.querySelector("#btn-tanslate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
 
 
-function ClickEventHandler() {
-    outputDiv.innerText = "zagshssjjj " + textInput.value;
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something is wrong with the server!! try again after sometime")
 }
 
 
 
-btntranslate.addEventListener("click", ClickEventHandler);
+function clickHandler() {
+
+
+    var inputText = textInput.value; //Taking input
+
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
+}
+
+
+
+btntranslate.addEventListener("click", clickHandler);
